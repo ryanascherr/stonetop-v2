48f2f78994ca1306b14fffda1a710bad7a90e3a3
ryanascherr <ryanascherr@gmail.com>
2021-06-25 13:30:43 -0400

    Began writing seed data for drives.

---
+++ seeds/seed.js
@@ -0,0 +1,35 @@
+const sequelize = require('../config/connection');
+const { User, Background, Drive, Character } = require('../models/');
+
+const userData = require('./userData.json');
+const backgroundData = require('./backgroundData.json');
+const driveData = require('./driveData.json');
+const characterData = require('./characterData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Character.bulkCreate(characterData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Background.bulkCreate(backgroundData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Drive.bulkCreate(driveData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
