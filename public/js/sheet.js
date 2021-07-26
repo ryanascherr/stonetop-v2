817927b9896e991110c4e1f15a8b54cfc40c5bc9
ryanascherr <ryanascherr@gmail.com>
2021-07-26 15:47:06 -0400

    Completed functionality for rolling normal stat rolls.

--- public/js/sheet.js
+++ public/js/sheet.js
@@ -1,8 +1,9 @@
-let rollOne;
-let rollTwo;
-
-$(".str").click(function () {
-    let stat = parseInt($(".str").text());
+$(".stat-button").click(function () {
+    let stat = parseInt($(this).text());
+    let statName = $(this).data("name");
+    console.log()
+    console.log("Success!");
+    console.log(stat);
     statRoll(stat);
     console.log(result);
     if (result > 9) {
@@ -12,12 +13,12 @@ $(".str").click(function () {
     } else {
         $(".level-of-success").text('Failure! Mark Experience...');
     }
-    $(".dice-result").text(`${rollOne} + ${rollTwo} + ${stat} = ${result}`);
+    $(".dice-result").text(`${rollOne} + ${rollTwo} + ${statName}(${stat}) = ${result}`);
 })
 
 function statRoll(stat) {
     rollOne = Math.floor(Math.random() * 6) + 1;
     rollTwo = Math.floor(Math.random() * 6) + 1;
-
     result = rollOne + rollTwo + stat;
 }
+
